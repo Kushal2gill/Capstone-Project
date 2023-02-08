bfc5f5f06943635d90eb6af4ecda494d43f92a00
Kushal <kushalgill00010@gmail.com>
2023-02-08 15:57:53 +0530

    final commit

--- src/Component/Note/Note.js
+++ src/Component/Note/Note.js
@@ -1,12 +1,59 @@
 import React from "react";
+import deleteIcon from "../../images/delete.png";
 import "./Note.css";
-
+let timer = 500,
+  timeout;
 export default function Note(props) {
+  const formatDate = (value) => {
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthName = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "may",
+      "june",
+      "July",
+      "August",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+    let hrs = date.getHours();
+    let amPm = hrs >= 12 ? "PM" : "AM";
+
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+    let day = date.getDate();
+    const month = monthName[date.getMonth()];
+    return `${hrs}:${min} ${amPm} ${day} ${month} `;
+  };
+
+  const debounce = (func) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, timer);
+  };
+  const updateText = (text, id) => {
+    debounce(() => props.updateText(text, id));
+  };
+
   return (
-    <div>
-      <div className="note" style={{ backgroundColor: props.note.color }}>
-        <textarea className="note_text" defaultValue={props.note.text} />
-        <p> {props.note.time}</p>
+    <div className="note" style={{ backgroundColor: props.note.color }}>
+      <textarea
+        className="note_text"
+        defaultValue={props.note.text}
+        onChange={(event) => updateText(event.target.value, props.note.id)}
+      />
+      <div className="note_footer">
+        <p>{formatDate(props.note.time)}</p>
+        <img
+          src={deleteIcon}
+          alt="DELETE"
+          onClick={() => props.deleteNote(props.note.id)}
+        />
       </div>
     </div>
   );
--- src/Component/NoteContainer/NoteContainer.js
+++ src/Component/NoteContainer/NoteContainer.js
@@ -3,14 +3,32 @@ import Note from "../Note/Note";
 import "./NoteContainer.css";
 
 export default function NoteContainer(props) {
+  const reverseArray = (arr) => {
+    const array = [];
+    for (let i = arr.length - 1; i >= 0; --i) {
+      array.push(arr[i]);
+    }
+    return array;
+  };
+  const notes = reverseArray(props.notes);
+
   return (
     <div className="Note-container">
       <h2>NOTES</h2>
 
       <div className="note-container_notes ">
-        {props.notes.map((item, index) => (
-          <Note key={index} note={item} />
-        ))}
+        {notes?.length > 0 ? (
+          notes.map((item) => (
+            <Note
+              key={item.id}
+              note={item}
+              deleteNote={props.deleteNote}
+              updateText={props.updateText}
+            />
+          ))
+        ) : (
+          <h3>No Notes Present</h3>
+        )}
       </div>
     </div>
   );
